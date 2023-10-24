9d404f1979ede4276f0bfaf85b38c7a9ff702586
NoorJehadKhawaja <noorkhj17@gmail.com>
2023-10-24 18:52:52 +0300

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <>
+    
+    <div>
+    <div class="container-fluid bg-black">
+          <h2 className='footer d-flex justify-content-center text-success'> END</h2>
+          </div>
+        </div>
+    </>
+    
+  );
+}
